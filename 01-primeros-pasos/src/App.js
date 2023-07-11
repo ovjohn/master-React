32215d38991cf55eea238442b217a7b12f0c73f8
John Olivares <olivaresvjohn@gmail.com>
2023-07-11 18:07:24 -0400

    Creando el Primer Componente

--- 01-primeros-pasos/src/App.js
+++ 01-primeros-pasos/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MiComponente from './MiComponente';
 
 function App() {
   return (
@@ -7,16 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Master en React 2023
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <MiComponente />
       </header>
     </div>
   );
---
+++ 01-primeros-pasos/src/MiComponente.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MiComponente = () =>{
+    return "Hola este es: <MiComponente />";
+}
+
+export default MiComponente;
