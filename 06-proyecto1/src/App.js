1c1633625b6797adde8f94f3c3c539869bb4d8ea
“John” <olivaresvjohn@gmail.com>
2023-08-25 15:28:33 -0400

    Creación de los Componentes Listado, Buscar, Crear

--- 06-proyecto1/src/App.js
+++ 06-proyecto1/src/App.js
@@ -1,84 +1,52 @@
+import { Buscador } from "./components/Buscador";
+import { Crear } from "./components/Crear";
+import { Listado } from "./components/Listado";
 
 function App() {
   return (
     <div className="layout">
-        
-    {/*Cabecera*/}
-    <header className="header">
+      {/*Cabecera*/}
+      <header className="header">
         <div className="logo">
-            <div className="play"></div>
+          <div className="play"></div>
         </div>
         <h1>MisPeliculas</h1>
-    </header>
+      </header>
 
-    {/*Barra navegadora*/}
-    <nav className="nav">
+      {/*Barra navegadora*/}
+      <nav className="nav">
         <ul>
-            <li><a href="/#">Inicio</a></li>
-            <li><a href="/#">Peliculas</a></li>
-            <li><a href="/#">Blog</a></li>
-            <li><a href="/#">Contacto</a></li>
+          <li>
+            <a href="/#">Inicio</a>
+          </li>
+          <li>
+            <a href="/#">Peliculas</a>
+          </li>
+          <li>
+            <a href="/#">Blog</a>
+          </li>
+          <li>
+            <a href="/#">Contacto</a>
+          </li>
         </ul>
-    </nav>
+      </nav>
 
-    {/*Contenido Principal*/}
-    <section className="content">
-        <article className="pelis-item">
-            <h3 className="title">Desarrollo Web</h3>
-            <p className="description">desarrolloweb.es</p>
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
+      {/*Contenido Principal*/}
+      <section className="content">
+        <Listado />
+      </section>
 
-        <article className="pelis-item">
-            <h3 className="title">Desarrollo Web</h3>
-            <p className="description">desarrolloweb.es</p>
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
+      {/*Barra lateral*/}
+      <aside className="lateral">
+        <Buscador />        
+        <Crear />
+      </aside>
 
-        <article className="pelis-item">
-            <h3 className="title">Desarrollo Web</h3>
-            <p className="description">desarrolloweb.es</p>
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
-
-        <article className="pelis-item">
-            <h3 className="title">Desarrollo Web</h3>
-            <p className="description">desarrolloweb.es</p>
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-        </article>
-    </section>
-
-    {/*Barra lateral*/}
-    <aside className="lateral">
-        <div className="search">
-            <h3 className="title">Buscador</h3>
-            <form>
-                <input type="text"/>
-                <button>Buscar</button>
-            </form>
-        </div>
-        <div className="add">
-            <h3 className="title">Añadir pelicula</h3>
-            <form>
-                <input type="text" placeholder="Titulo"/>
-                <textarea placeholder="Descripcion"></textarea>
-                <input type="submit" value="Guardar" />
-            </form>
-
-        </div>
-
-    </aside>
-
-    {/*Pie de PAgina*/}
-    <footer className="footer">
+      {/*Pie de PAgina*/}
+      <footer className="footer">
         &copy; Máster en JavaScript ES12 y Typescript
-    </footer>
-
-</div>
+      </footer>
+    </div>
   );
 }
 
---
+++ 06-proyecto1/src/components/Listado.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export const Listado = () => {
+  return (
+    <>
+        <article className="pelis-item">
+          <h3 className="title">Desarrollo Web</h3>
+          <p className="description">desarrolloweb.es</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="pelis-item">
+          <h3 className="title">Desarrollo Web</h3>
+          <p className="description">desarrolloweb.es</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="pelis-item">
+          <h3 className="title">Desarrollo Web</h3>
+          <p className="description">desarrolloweb.es</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+
+        <article className="pelis-item">
+          <h3 className="title">Desarrollo Web</h3>
+          <p className="description">desarrolloweb.es</p>
+          <button className="edit">Editar</button>
+          <button className="delete">Borrar</button>
+        </article>
+    </>
+  )
+}
