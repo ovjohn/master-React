5b87a7a994c251e219ac7f2fe423ee907ef2d9c2
“John” <olivaresvjohn@gmail.com>
2023-08-28 18:13:47 -0400

    añadiendo useState en el componente Crear

--- 06-proyecto1/src/components/Crear.js
+++ 06-proyecto1/src/components/Crear.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Crear = () => {
+  let title = "Añadir pelicula";
+
+  const [pelisState, setPelisState] = useState({
+    titulo:'',
+    descripcion:''
+  });
+
+  const conseguirDatosFrom = e => {
+    e.preventDefault();
+
+    let target = e.target;
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+
+    let pelis ={
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    }
+
+    setPelisState(pelis);
+    console.log(pelisState);
+  }
+
+
+
+
   return (
     <>
         <div className="add">
-          <h3 className="title">Añadir pelicula</h3>
-          <form>
-            <input type="text" placeholder="Titulo" />
-            <textarea placeholder="Descripcion"></textarea>
+          <h3 className="title">{title}</h3>
+
+          {pelisState.titulo}
+          <form onSubmit={conseguirDatosFrom}>
+            <input type="text" placeholder="Titulo" name="titulo"/>
+            <textarea placeholder="Descripcion" name="descripcion"></textarea>
             <input type="submit" value="Guardar" />
           </form>
         </div>
