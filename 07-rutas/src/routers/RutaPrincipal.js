c133f42a837e0d4501de2e9a574e1027731853b8
“John” <olivaresvjohn@gmail.com>
2023-10-16 15:53:34 -0400

    varios update

--- 07-rutas/src/routers/RutaPrincipal.js
+++ 07-rutas/src/routers/RutaPrincipal.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, NavLink, BrowserRouter } from 'react-router-dom'
 import { Inicio } from '../components/Inicio'
 import { Articulos } from '../components/Ariculos'
 import { Contacto } from '../components/Contacto'
@@ -9,14 +9,33 @@ import { Error } from '../components/Error'
 export const RutaPrincipal = () => {
 
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Inicio/>}/>
-            <Route path='/inicio' element={<Inicio/>}/>
-            <Route path='/articulos' element={<Articulos />}/>
-            <Route path='/contacto' element={<Contacto />}/>
-            <Route path='*' element={<Error />} />
-        </Routes>
-    </BrowserRouter>
+         
+      <BrowserRouter>
+          <h1>Cabecera Principal</h1>
+          <hr />
+          <nav>
+            <ul>
+              <li><NavLink to="/inicio">Inicio</NavLink></li>
+              <li> <NavLink to="/articulos">Articulos</NavLink></li>
+              <li><NavLink to="/contacto">Contacto</NavLink></li>
+            </ul>
+          </nav>      
+          <hr />
+          <section className='contenido_principal'>
+            <Routes>
+                <Route path='/' element={<Inicio/>}/>
+                <Route path='/inicio' element={<Inicio/>}/>
+                <Route path='/articulos' element={<Articulos />}/>
+                <Route path='/contacto' element={<Contacto />}/>
+                <Route path='*' element={<Error />} />
+            </Routes>
+          </section>
+          
+          <footer>
+            <hr />
+            <h2>Aqui el Pie de Pagina</h2>
+          </footer>
+      </BrowserRouter>     
+    
   )
 }
