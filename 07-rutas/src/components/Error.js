aa33c4180ff3f705b02cce2d6aa139816bbf0ecd
“John” <olivaresvjohn@gmail.com>
2023-10-16 17:06:47 -0400

    Creando un link SPA en la pagina de Error

--- 07-rutas/src/components/Error.js
+++ 07-rutas/src/components/Error.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Error = () => {
   return (    
         <>
             <h1>Error 404</h1>
             <strong>Esta pagina no exixte</strong>
+            <Link to='/inicio'>volver a la pagina principal</Link>
         </>        
   )
 }
